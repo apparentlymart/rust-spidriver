ae4b2258ff76e9f8043336bcdecde65e8a988801
Martin Atkins <mart@degeneration.co.uk>
2019-06-28 17:54:40 -0700

    spidriver: Documentation

--- spidriver/src/lib.rs
+++ spidriver/src/lib.rs
@@ -1,7 +1,36 @@
+//! SPIDriver client library
+//!
+//! This library implements the [SPIDriver](https://spidriver.com/) protocol,
+//! allowing Rust programs to interact with an SPIDriver device and in turn
+//! to interact with SPI devices.
+//!
+//! The entry point is `SPIDriver::new`, which takes (and consumes) a serial
+//! writer and a serial reader as defined by
+//! [`embedded_hal::serial`](https://docs.rs/embedded-hal/0.2.3/embedded_hal/serial/).
+//! If you are running on a general computing platform then you can use
+//! [`serial_embedded_hal`](https://docs.rs/serial-embedded-hal/0.1.2/serial_embedded_hal/struct.Serial.html)
+//! to connect with a serial port provided by your operating system:
+//!
+//! ```rust
+//! let port = Serial::new(
+//!     "/dev/ttyUSB0",
+//!     &PortSettings {
+//!         baud_rate: serial_embedded_hal::BaudRate::BaudOther(460800),
+//!         char_size: serial_embedded_hal::CharSize::Bits8,
+//!         parity: serial_embedded_hal::Parity::ParityNone,
+//!         stop_bits: serial_embedded_hal::StopBits::Stop1,
+//!         flow_control: serial_embedded_hal::FlowControl::FlowNone,
+//!     },
+//! )?;
+//! let (tx, rx) = port.split();
+//! let sd = SPIDriver::new(tx, rx);
+//! ```
+
 #![no_std]
 
 use embedded_hal::serial;
 
+/// `SPIDriver` represents a connected SPIDriver device.
 #[derive(Debug)]
 pub struct SPIDriver<TX: serial::Write<u8>, RX: serial::Read<u8>> {
     ch: Channel<TX, RX>,
@@ -12,12 +41,19 @@ where
     TX: serial::Write<u8, Error = TXErr>,
     RX: serial::Read<u8, Error = RXErr>,
 {
+    /// `new` consumes a serial `Write` and `Read` implementation to produce
+    /// an `SPIDriver` object.
     pub fn new(tx: TX, rx: RX) -> Self {
         Self {
             ch: Channel { tx: tx, rx: rx },
         }
     }
 
+    /// `echo` asks the SPIDriver to echo back the given character.
+    ///
+    /// This method can be useful for detecting whether the remote device on
+    /// the serial line is actually a SPIDriver: ask it to echo back a few
+    /// bytes and verify that it does.
     pub fn echo(&mut self, ch: u8) -> Result<u8, Error<TXErr, RXErr>> {
         self.ch.write(b'e')?;
         self.ch.write(ch)?;
@@ -25,16 +61,19 @@ where
         self.ch.read()
     }
 
+    /// `select` asserts the chip select signal by driving it low.
     pub fn select(&mut self) -> Result<(), Error<TXErr, RXErr>> {
         self.ch.write(b's')?;
         self.ch.flush()
     }
 
+    /// `unselect` de-asserts the chip select signal by driving it high.
     pub fn unselect(&mut self) -> Result<(), Error<TXErr, RXErr>> {
         self.ch.write(b'u')?;
         self.ch.flush()
     }
 
+    /// `set_a` sets the active state of the auxillary "A" pin on the SPIDriver.
     pub fn set_a(&mut self, high: bool) -> Result<(), Error<TXErr, RXErr>> {
         self.ch.write(b'a')?;
         if high {
@@ -45,6 +84,7 @@ where
         self.ch.flush()
     }
 
+    /// `set_b` sets the active state of the auxillary "B" pin on the SPIDriver.
     pub fn set_b(&mut self, high: bool) -> Result<(), Error<TXErr, RXErr>> {
         self.ch.write(b'b')?;
         if high {
@@ -55,11 +95,19 @@ where
         self.ch.flush()
     }
 
+    /// `disconnect` requests that the SPIDriver disconnect from the SPI signals,
     pub fn disconnect(&mut self) -> Result<(), Error<TXErr, RXErr>> {
         self.ch.write(b'x')
     }
 
+    /// `write` sends up to 64 bytes out over the SPIDriver's MOSI line.
+    ///
+    /// If the given slice is longer than 64 bytes then `write` will return
+    /// the `Request` error.
     pub fn write(&mut self, data: &[u8]) -> Result<(), Error<TXErr, RXErr>> {
+        if data.len() == 0 {
+            return Ok(()); // nothing to do
+        }
         if data.len() > 64 {
             return Err(Error::Request);
         }
@@ -71,7 +119,19 @@ where
         Ok(())
     }
 
+    /// `transfer` sends up to 64 bytes out over the SPIDriver's MOSI line,
+    /// and returns the data returned by the target device.
+    ///
+    /// `transfer` modifies the given array in-place, replacing each byte
+    /// with the corresponding byte returned from the device. It then returns
+    /// a slice with the same backing array.
+    ///
+    /// If the given slice is longer than 64 bytes then `write` will return
+    /// the `Request` error.
     pub fn transfer<'v>(&mut self, data: &'v mut [u8]) -> Result<&'v [u8], Error<TXErr, RXErr>> {
+        if data.len() == 0 {
+            return Ok(data); // nothing to do
+        }
         if data.len() > 64 {
             return Err(Error::Request);
         }
@@ -86,6 +146,10 @@ where
         Ok(data)
     }
 
+    // `write_byte` is like `write` but writes only a single byte.
+    //
+    // This is a convenience helper to avoid constructing an array and a slice
+    // from that array just to send one byte.
     pub fn write_byte(&mut self, b: u8) -> Result<(), Error<TXErr, RXErr>> {
         self.ch.write(0xc0)?;
         self.ch.write(b)
@@ -116,6 +180,7 @@ where
     }
 }
 
+/// `Error` represents communication errors.
 #[derive(Debug)]
 pub enum Error<TXErr, RXErr> {
     /// `Protocol` indicates that the library receieved an invalid or unexpected
